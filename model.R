394ffa8e0da1664d74a17fdb0c3b7210a52acb08
Rahul Singh Pundir <90499624+RahulSinghPundir@users.noreply.github.com>
2022-06-09 21:08:51 +0530

    Update model.R

--- model.R
+++ model.R
@@ -8,7 +8,7 @@ set.seed(1234)
 
 # load the data
 traincsv <- read.csv("https://github.com/RahulSinghPundir/couresra/blob/main/pml-training.csv")
-testcsv <- read.csv("https://github.com/b-zhang93/Practical-Machine-Learning-Project/blob/master/data/pml-testing.csv")
+testcsv <- read.csv("https://github.com/RahulSinghPundir/couresra/blob/main/pml-training.csv")
 
 # cleaning the data
 traincsv <- traincsv[,colMeans(is.na(traincsv)) < .9] #removing mostly na columns
